12b492758db472c31227b1bd7c694ce5918b04b6
Pedro-Arantes <developer9pedro@gmail.com>
2022-11-17 00:46:53 -0300

    adding sign in and sign up

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,16 +1,54 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState, useContext } from 'react';
+import { DataContext } from "../context/Auth";
 import axios from "axios";
 import { MainStyled, FormStyled, BtnStyle } from '../style/LogCadStyle';
 
 export default function LoginPage() {
 
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [status, setStatus] = useState(false)
+    const { setData } = useContext(DataContext)
     const navigate = useNavigate();
 
     const submit = (e) => {
-        //VerificaLogin();
+        VerificaLogin();
         e.preventDefault();
-        navigate("/home")
+        //navigate("/home")
+    }
+
+    const VerificaLogin = () => {
+
+        const obj = {
+            email: email,
+            password: senha
+        }
+
+        const tratarSucesso = (resposta) => {
+
+            const token = resposta.data.token
+            console.log(resposta)
+            setData(token)
+            
+            navigate("/home")
+        }
+
+        const tratarErro = (resp) => {
+
+            alert(resp.response.data.message)
+            setStatus(false)
+            setEmail("")
+            setSenha("")
+
+        }
+
+
+        const requisicao = axios.post('http://localhost:5000/sign-in', obj);
+        requisicao.then(tratarSucesso)
+        requisicao.catch(tratarErro)
+        setStatus(true)
+
     }
 
 
@@ -18,8 +56,8 @@ export default function LoginPage() {
         <MainStyled>
             <h1>MyWallet</h1>
             <FormStyled onSubmit={submit}>
-                <input   required placeholder='E-mail' type="email" />
-                <input  required placeholder='Senha'  type="password" />
+                <input   required disabled={status} onChange={e => setEmail(e.target.value)}  value={email} placeholder='E-mail' type="email" />
+                <input  required placeholder='Senha' onChange={e => setSenha(e.target.value)}  value={senha} type="password" />
                 <BtnStyle >Entrar</BtnStyle>
             </FormStyled>
             <Link to='/cadastro'>Primeira vez? Cadastre-se!</Link>
--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -3,18 +3,69 @@ import { useState, useContext } from 'react';
 import axios from "axios";
 import { MainStyled, FormStyled, BtnStyle } from '../style/LogCadStyle';
 
-export default function LoginPage() {
+export default function RegisterPage() {
 
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
+    const [senhaConf, setConf] = useState('')
+    const [status, setStatus] = useState(false)
+
+    const navigate = useNavigate();
+    const submit = (e) => {
+        VerificaCadastro();
+        e.preventDefault();
+        //navigate("/")
+    }
+
+    const VerificaCadastro = () => {
+
+        if (senha === senhaConf) {
+            const obj = {
+                name: name,
+                email: email,
+                password: senha
+            }
+    
+            const tratarSucesso = (resposta) => {
+                console.log(resposta)
+    
+                navigate("/")
+    
+            }
+            const tratarErro = (resp) => {
+                console.log(resp)
+                alert(resp.response.data)
+                setStatus(false)
+            }
+    
+    
+            const requisicao = axios.post('http://localhost:5000/sign-up', obj);
+            requisicao.then(tratarSucesso)
+            requisicao.catch(tratarErro)
+            setStatus(true)
+        }else{
+            alert("As Senhas não coincidem!,favor confirmar a senha corretamente")
+            setSenha("")
+            setConf("")
+        }
+
+        
+    }
 
     return (
         <MainStyled>
             <h1>MyWallet</h1>
-            <FormStyled>
-                <input   required placeholder='Nome' type="text" />
-                <input   required placeholder='E-mail' type="email" />
-                <input  required placeholder='Senha'  type="password" />
-                <input  required placeholder='Confirme a senha'  type="password" />
-                <BtnStyle >Entrar</BtnStyle>
+            <FormStyled onSubmit={submit}>
+                <input required placeholder='Nome' onChange={e => setName(e.target.value)} disabled={status} value={name} type="text" />
+
+                <input required placeholder='E-mail' onChange={e => setEmail(e.target.value)} value={email} disabled={status} type="email" />
+
+                <input required placeholder='Senha' onChange={e => setSenha(e.target.value)} disabled={status} value={senha} type="password" />
+
+                <input required  onChange={e => setConf(e.target.value)} disabled={status} value={senhaConf} placeholder='Confirme a senha' type="password" />
+
+                <BtnStyle >Cadastrar</BtnStyle>
             </FormStyled>
             <Link to='/'>Já tem uma conta? Entre agora!</Link>
         </MainStyled>
